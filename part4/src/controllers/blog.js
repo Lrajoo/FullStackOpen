42ae9105b804bcd1463a85eeef94175c087b9f17
Lingess Rajoo <Lrajoo@purdue.edu>
2021-06-22 14:21:10 -0700

    Completed 4.13-4.14

--- part4/src/controllers/blog.js
+++ part4/src/controllers/blog.js
@@ -26,4 +26,26 @@ blogsRouter.post('/', (request, response) => {
     .catch(error => next(error));
 });
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  };
+
+  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON());
+    })
+    .catch(error => next(error));
+});
+
 module.exports = blogsRouter;
